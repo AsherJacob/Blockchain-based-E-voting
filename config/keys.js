edbfcec826fb03788a1b3d8e520b4ca42f2131ab
Asher <asherjcb@gmail.com>
2020-01-16 09:40:36 +0530

    3rd commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://asherjcb@gmail.com:'+ encodeURIComponent('as14h4er98') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
