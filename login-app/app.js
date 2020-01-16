122ae9aac87137a7d49e2f186562966d7d1a7b5b
Debin <debinptpm@gmail.com>
2020-01-16 11:45:25 +0530

    front-end updates

--- login-app/app.js
+++ login-app/app.js
@@ -54,7 +54,8 @@ app.use(function(req, res, next) {
 });
 
 // Routes
-app.use('/', require('./routes/index.js'));
+app.use(express.static("front-end"));
+//app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 5000;
