a897bc12023255928880bc1d0c601735876c69cc
Asher <asherjcb@gmail.com>
2020-01-16 10:47:37 +0530

    login5

--- login-app/config/keys.js
+++ login-app/config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://asherjcb@gmail.com:'+ encodeURIComponent('as14h4er98') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb+srv://Udaya:evoting123@cluster0-upthn.mongodb.net/test?retryWrites=true&w=majority'
 
 module.exports = {
     mongoURI: dbPassword
