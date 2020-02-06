fea89bbe179b3523333ad3a89dd75056907579c2
Udaya Shanker M <52904898+udayashanker97@users.noreply.github.com>
2020-02-06 12:24:36 +0530

    Add files via upload

---
+++ RegApp.js
@@ -0,0 +1,152 @@
+const express = require('express');
+const path = require('path');
+const User = require('./models/User');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('connect-flash');
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+
+
+
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// For static front end
+app.use(express.static('front-end'));
+
+
+//GET Login
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname,'front-end','login.html')));
+
+//GET Registration page
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname,'front-end','register.html')));
+
+
+// Registration
+app.post('/register', (req,res) => {
+
+  const { name, email, password, password2 } = req.body;
+
+  if(password !== password2){
+    res.send("Passwords do not match");
+  }
+
+  else if(password.length < 6){
+    res.send("Password must be at least 6 characters");
+  }
+  
+  else{
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        res.send("Email already exists");
+      }
+
+      else{
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+
+        bcrypt.genSalt(10, (err,salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save().then( () => {res.redirect('/login');}).catch(err => console.log(err));
+            
+  
+              
+          });
+        });
+    }
+
+  });
+}
+
+});
+
+
+ //Function to ensure you're logged in before seeing dashboard
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  else
+     res.redirect('/login');
+}
+
+
+//GET Dashboard
+app.get('/dashboard',ensureAuthenticated, (req,res) => res.sendFile(path.join(__dirname,'front-end','dashboard.html')));
+
+app.get('/vote', (req,res) => res.sendFile(path.join(__dirname,'blockchain','src/index.html')) );
+
+//login
+
+app.post('/login',(req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/vote',
+    failureRedirect: res.send('Invalid login credentials')
+  })(req, res, next);
+});
+
+
+
+//logout
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+    
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
